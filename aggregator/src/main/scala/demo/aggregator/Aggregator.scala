1c5211598d26c30bde09134765924b03f2d6530d
geekbytes0xff <0xff@geekbytes.io>
2018-05-01 22:30:22 +0100

    pretty print in aggregator actor

--- aggregator/src/main/scala/demo/aggregator/Aggregator.scala
+++ aggregator/src/main/scala/demo/aggregator/Aggregator.scala
@@ -3,17 +3,20 @@ package demo.aggregator
 import akka.actor.{Actor, ActorRef, Props}
 import akka.persistence.{PersistentActor, RecoveryCompleted}
 import com.typesafe.scalalogging.LazyLogging
+import demo.protocols.Utils
 import demo.protocols.aggregator.commands.AddNumber
 import demo.protocols.aggregator.events.NumberAdded
 import demo.protocols.aggregator.state.Aggregate
-import io.circe._, io.circe.generic.auto._, io.circe.syntax._
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.syntax._
 
 object Aggregator {
   def props(): Props = Props(new Aggregator())
 
 }
 
-class Aggregator() extends PersistentActor with LazyLogging {
+class Aggregator() extends PersistentActor with LazyLogging with Utils {
 
   var state: Aggregate = Aggregate(0)
 
@@ -25,18 +28,18 @@ class Aggregator() extends PersistentActor with LazyLogging {
 
 
     case x@AddNumber(number) => {
-      logger.info("Aggregator Received Command {}", x.asJson.noSpaces)
+      logger.info("Aggregator Received Command {}", prettyPrint(x))
       persist[NumberAdded](NumberAdded(number))(update)
     }
     case x => {
-      logger.info("Aggregator Received UnKnown Command {}", x.asJson.noSpaces)
+      logger.info("Aggregator Received UnKnown Command {}", x)
     }
   }
 
   override def receiveRecover: Receive = {
     case x: NumberAdded => update(x)
     case RecoveryCompleted => {
-      logger.info("Aggregator Recovery Complete State {}", state.asJson.noSpaces)
+      logger.info("Aggregator Recovery Complete State {}", prettyPrint(state))
     }
   }
 
