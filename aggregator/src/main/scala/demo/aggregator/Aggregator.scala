4d02165211dec783fae9f3b8f5c2be612ae23234
geekbytes0xff <0xff@geekbytes.io>
2018-05-02 11:46:15 +0100

    fixed issue with Proto messages

--- aggregator/src/main/scala/demo/aggregator/Aggregator.scala
+++ aggregator/src/main/scala/demo/aggregator/Aggregator.scala
@@ -4,9 +4,9 @@ import akka.actor.Props
 import akka.persistence.{PersistentActor, RecoveryCompleted}
 import com.typesafe.scalalogging.LazyLogging
 import demo.protocols.Utils
-import demo.protocols.aggregator.commands.SumNumber
-import demo.protocols.aggregator.events.NumberSummed
-import demo.protocols.aggregator.state.Sum
+import demo.protocols.aggregator.commands.AddNumber
+import demo.protocols.aggregator.events.NumberAdded
+import demo.protocols.aggregator.state.Aggregate
 import io.circe._, io.circe.generic.auto._, io.circe.syntax._
 
 object Aggregator {
@@ -14,15 +14,13 @@ object Aggregator {
 
 }
 
-
-
 class Aggregator() extends PersistentActor with LazyLogging with Utils {
 
-  var state: Sum = Sum(0)
+  var state: Aggregate = Aggregate(0)
 
   def update(event: Any):Unit = event match {
-    case e@NumberSummed(number) => {
-      state = state.copy(state.total + number)
+    case e@NumberAdded(number) => {
+      state = state.copy(state.value + number)
       logger.info("\n---- updating state ---------\nevent: {}\nnew state: {}\n --------------------", prettyPrint(e),prettyPrint(state))
     }
     case _=> ()
@@ -31,9 +29,9 @@ class Aggregator() extends PersistentActor with LazyLogging with Utils {
   override def receiveCommand: Receive = {
 
 
-    case x@SumNumber(number) => {
+    case x@AddNumber(number) => {
       logger.info("\n----Aggregator Received Command---------\n{}\n--------------------", prettyPrint(x))
-      persist[NumberSummed](NumberSummed(number))(update)
+      persist[NumberAdded](NumberAdded(number))(update)
     }
     case x => {
       logger.info("Aggregator Received UnKnown Command {}", x)
@@ -41,7 +39,7 @@ class Aggregator() extends PersistentActor with LazyLogging with Utils {
   }
 
   override def receiveRecover: Receive = {
-    case x: NumberSummed => update(x)
+    case x: NumberAdded => update(x)
     case RecoveryCompleted => {
       logger.info("\n----Aggregator Recovery Complete with State---------\n{}\n-----------------", prettyPrint(state))
     }
